03b66072fc0932188f59d0c908e1e5f181c03b64
Sandeep <sandeepmenta172@gmail.com>
2021-09-10 20:56:13 +0530

    changes related to event form added to the app

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,45 @@
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import { lazy, Suspense } from 'react';
-import { Provider } from 'react-redux';
+import { lazy, Suspense } from "react";
+import { Provider } from "react-redux";
 
-import './App.scss';
-import AppLayout from './layout';
+import "./App.scss";
+import AppLayout from "./layout";
 import configureStore from "./store";
 
-const LoginComponent = lazy(() => import('./pages/login'));
-const RegisterComponent = lazy(() => import('./pages/register'));
-const DashboardComponent = lazy(() => import('./pages/dashboard'));
+const LoginComponent = lazy(() => import("./pages/login"));
+const RegisterComponent = lazy(() => import("./pages/register"));
+const DashboardComponent = lazy(() => import("./pages/dashboard"));
+const EventFormComponent = lazy(() => import("./pages/EventForm"));
 
 function App() {
   return (
     <div className="App">
       <Provider store={configureStore}>
-      <Suspense fallback={<p>Loading...</p>}>
-        <Router>
-          <Switch>
-          <Route path="/" exact><LoginComponent /></Route>
-            <Route exact path="/login"><LoginComponent /></Route>
-            <Route exact path="/register"><RegisterComponent /></Route>
-            <Route exact path="/dashboard">
-              <AppLayout>
-                <DashboardComponent />
-              </AppLayout>
-            </Route>
-            <Route exact path="/events">
-              <AppLayout />
-            </Route>
-          </Switch>
-        </Router>
-      </Suspense>
+        <Suspense fallback={<p>Loading...</p>}>
+          <Router>
+            <Switch>
+              <Route path="/" exact>
+                <LoginComponent />
+              </Route>
+              <Route exact path="/login">
+                <LoginComponent />
+              </Route>
+              <Route exact path="/register">
+                <RegisterComponent />
+              </Route>
+              <Route exact path="/dashboard">
+                <AppLayout>
+                  <DashboardComponent />
+                </AppLayout>
+              </Route>
+              <Route exact path="/add-events">
+                <AppLayout>
+                  <EventFormComponent />
+                </AppLayout>
+              </Route>
+            </Switch>
+          </Router>
+        </Suspense>
       </Provider>
     </div>
   );
